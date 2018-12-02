f668990b2c622f79e8a9c1d892de6f0c80da2801
milesgreene0 <44852291+milesgreene0@users.noreply.github.com>
2018-12-02 17:58:41 -0600

    Update Level5.cpp

--- Level5.cpp
+++ Level5.cpp
@@ -15,10 +15,12 @@ int main(int argc, char ** argv)
     int R,G,B;
 
     //RGB values for red (Can change to what ever color though)
-    // Black BAckground.
+    // Black BAckground. If you need to change the colors you can go to html colors to change the rgb.
     R = 0;
 	G = 0;
     B = 0;
+    
+    // R is red, G is green, and b is Blue. These colors can create any color. 
 
 
 
@@ -26,11 +28,12 @@ int main(int argc, char ** argv)
     {
 		if(!stopped){
 
-//Sets position of the square (If you go below 110 or over 830 the square is off the sdl screen at least on my computer)
+//Sets position of the square (If you go below 110 or over 830 the square is off the sdl screen at least on my computer) the 0.001 is so it covers the whole screen has the natural color of the background. 
             x = 0.001;
 			y = 0.001;
 
 
+// the 1000 in the x and Y is for placement of the back ground and 1000, 1000 covers everything. 
 
 			for(xd = 0; xd < 1000 && x + xd < g.getCol(); xd++ ){
 				for(yd = 0; yd < 1000 && y + yd < g.getRow(); yd++ ){
